d170b14028050215d09a6d00e5a074c5f107b920
kishorekala <157871447+kishorekala@users.noreply.github.com>
2024-02-13 15:15:57 +0000

    New page routing  changes

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine, text
 import os
 
-
 connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(connection_string, echo=True)
@@ -13,3 +12,16 @@ def load_jobs_from_db():
     rows = result.all()
     db_jobs = [row._asdict() for row in rows]
   return db_jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as connection:
+    result = connection.execute(text("select * from jobs where id = :val"),
+                                {"val": id})
+
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      db_row = rows[0]._asdict()
+  return db_row
